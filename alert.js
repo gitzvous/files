a12d83a24cdab367a8deb37135592977b1162e23
gitzvous <165530816+gitzvous@users.noreply.github.com>
2024-04-23 18:59:22 +0200

    Create alert.js

---
+++ alert.js
@@ -0,0 +1,5 @@
+document.addEventListener("DOMContentLoaded", function() {
+    setTimeout(function() {
+        alert('Hello! This alert appeared 2 seconds after the page loaded.');
+    }, 2000); // 2000 milliseconds = 2 seconds
+});
